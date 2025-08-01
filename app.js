173b75c5892ee87d874262c33b10e1c25bf382ff
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-08-01 13:49:50 +0200

    feat: Refactor app.js to include posts router and static file serving

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('public'))
 const posts = require('./posts.js')
+const postsRouter = require('./routers/posts.js')
+app.use(express.static('public'))
+
+app.use('/posts', postsRouter)
 
 app.get('/', (req, res) => {
   res.send('Server del mio blog')
